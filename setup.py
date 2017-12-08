36b5476e80a34d302e214df7a62f91b12e30103a
Allison <allison@Marshmallows-Dell.local>
2017-12-08 08:56:17 -0600

    some udpates

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='RetroBrowser',
-      version='0.1',
-      description='A Python implementation of a text-based Grails',
-      author='Allison F',
-      author_email='yetanotherallisonf@yahoo.com',
-      url='https://github.com/allisonf/retro-browser',
-      packages=['retrobrowser.framework',
-                'retrobrowser.framework.defaultapp',
-                'retrobrowser.framework.defaultapp.controllers',
-                'retrobrowser.framework.defaultapp.views.Error400',
-                'retrobrowser.framework.defaultapp.views.Error404',
-                'retrobrowser.framework.defaultapp.views.Error500'],
-      scripts=[
-          'bin/retroBrowser'
-      ]
-     )
+setup(
+    name='RetroBrowser',
+    version='0.1',
+    description='A Python implementation of a text-based Grails',
+    author='Allison F',
+    author_email='yetanotherallisonf@yahoo.com',
+    url='https://github.com/allisonf/retro-browser',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Education',
+        'Topic :: Games/Entertainment',
+        'Topic :: Terminals',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='mvc command line text based games',
+
+    packages=find_packages(exclude=['docs', 'tests']),
+
+    scripts=[
+      'bin/retroBrowser'
+    ],
+    install_requires=["ConventionCrawler"]
+)
